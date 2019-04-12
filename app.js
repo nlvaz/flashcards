e28af81be4b91b5ee0ec3d7bbefdde2f944ef108
Noelia Vaz <nlvazquez01@gmail.com>
2019-04-12 09:03:37 -0400

    sandbox.pug added

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 
+const names = [
+	{First: "Leilani", Last: "Vazquez"},
+	{First: "Noelia", Last: "Vazquez"},
+	{First: "Carmen", Last: "Vazquez"},
+	{First: "German", Last: "Vazquez"},
+	{First: "Ashley", Last: "Dzergoski"},
+	{First: "NJIT", Last: "2023"},
+	{First: "Debbie"},
+	{Last: "James"}
+];
+
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
@@ -9,7 +20,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/cards', (req, res) => {
-	res.render('card');
+	res.render('card', {prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is!"});
+});
+
+// /sandbox : challenge to create random path and use iteration to display first and last name from an array into two table column :)
+// First Name | Last Name
+app.get('/sandbox', (req, res) => {
+	res.render('sandbox', {prompt: names})
 });
 
 app.listen(3000, () => {
